81c675df18cd81f89b6f21ef9f74dd5907babaf2
Guillermo St <guillermo_stancanelli@outlook.com>
2022-04-09 11:24:18 -0300

    Initial structure by cargo new

---
+++ target/rls/debug/deps/diff-21c6bb3b1e30f0a9.d
@@ -0,0 +1,5 @@
+/home/guillermo/Code/rust/diff/target/rls/debug/deps/diff-21c6bb3b1e30f0a9.rmeta: src/main.rs
+
+/home/guillermo/Code/rust/diff/target/rls/debug/deps/diff-21c6bb3b1e30f0a9.d: src/main.rs
+
+src/main.rs:

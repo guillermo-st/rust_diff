578a892b37ac6838d1f86fba561c52845f3769c5
Guillermo St <guillermo_stancanelli@outlook.com>
2022-04-09 16:37:35 -0300

    /target should not be version controlled

--- target/rls/debug/deps/diff-53aa3bb016e7ec32.d
+++
@@ -1,5 +0,0 @@
-/home/guillermo/Code/rust/diff/target/rls/debug/deps/diff-53aa3bb016e7ec32.rmeta: src/main.rs
-
-/home/guillermo/Code/rust/diff/target/rls/debug/deps/diff-53aa3bb016e7ec32.d: src/main.rs
-
-src/main.rs:

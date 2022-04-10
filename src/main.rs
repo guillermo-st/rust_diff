3225230d3ef81b75923a4b4eaebf771b3f3cef80
Guillermo St <guillermo_stancanelli@outlook.com>
2022-04-10 15:54:49 -0300

    added recursive diff function

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,38 @@ use std::cmp;
 use std::fmt;
 
 #[derive(Debug)]
-struct LCS {
+struct Lcs<'t> {
     grid: Vec<Vec<u8>>,
+    first_sequence : &'t[String],
+    second_sequence : &'t[String],
 }
 
-impl LCS {
-    fn new(first_sequence: &[String], second_sequence: &[String]) -> LCS {
+fn diff_rec(lcs: &Lcs, i: usize, j: usize){
+    if i>0 && j>0 && lcs.first_sequence[i-1] == lcs.second_sequence[j-1]{
+        diff_rec(lcs, i-1, j-1);
+        println!("  {}", lcs.first_sequence[i-1]);
+    }
+    else if j>0 && (i==0 || lcs.grid[i][j-1] >= lcs.grid[i-1][j]){
+        diff_rec(lcs, i, j-1);
+        println!("> {}", lcs.second_sequence[j-1]);
+    }
+    else if i>0 && (j==0 || lcs.grid[i][j-1] < lcs.grid[i-1][j]){
+        diff_rec(lcs, i-1, j);
+        println!("< {}", lcs.first_sequence[i-1]);
+    }
+    else{
+        println!();
+    }
+}
+
+impl Lcs<'_> {
+    fn new<'t>(first_sequence: &'t[String], second_sequence: &'t[String]) -> Lcs<'t> {
         let first_len = first_sequence.len();
         let sec_len = second_sequence.len();
         let mut grid = vec![vec![0; sec_len + 1]; first_len + 1];
 
         for i in 0..first_len {
-            for j in 0..sec_len {
+            for (j, _) in second_sequence.iter().enumerate() {
                 if first_sequence[i] == second_sequence[j] {
                     grid[i + 1][j + 1] = grid[i][j] + 1;
                 } else {
@@ -21,11 +41,15 @@ impl LCS {
                 }
             }
         }
-        LCS { grid }
+        Lcs {
+            grid,
+            first_sequence,
+            second_sequence
+        }
     }
 }
 
-impl fmt::Display for LCS {
+impl fmt::Display for Lcs<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut grid_string = String::new();
 
@@ -39,6 +63,7 @@ impl fmt::Display for LCS {
 fn main() {
     let hello1 = vec![
         String::from("hello"),
+        String::from("horrible"),
         String::from("world"),
         String::from("!"),
     ];
@@ -49,6 +74,8 @@ fn main() {
         String::from("!"),
     ];
 
-    let a_grid = LCS::new(&hello1, &hello2);
+    let a_grid = Lcs::new(&hello1, &hello2);
     println!("{}", a_grid);
+
+    diff_rec(&a_grid, a_grid.first_sequence.len(), a_grid.second_sequence.len())
 }

68f3fcea7445bfb7fbf9eb9364182a56e1ce0366
Guillermo St <guillermo_stancanelli@outlook.com>
2022-04-10 19:44:47 -0300

    Properly read 2 files to generate the diff

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::env;
+
 mod lcs;
 
+fn read_file_lines(filename: &str) -> io::Result<Vec<String>>{
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
 fn main() {
-    let hello1 = vec![
-        String::from("hello"),
-        String::from("horrible"),
-        String::from("world"),
-        String::from("!"),
-    ];
-    let hello2 = vec![
-        String::from("hello"),
-        String::from("beautiful"),
-        String::from("world"),
-        String::from("!"),
-    ];
 
-    let a_grid = lcs::Lcs::new(&hello1, &hello2);
-    println!("{}", a_grid);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3{
+        panic!("[ERR] Invalid number of arguments to generate diff. Usage: $ diff <filename1> <filename2>");
+    }
 
-    a_grid.diff();
+    let first_filename = &args[1].as_str();
+    let second_filename = &args[2].as_str();
+
+    let first_sequence = read_file_lines(first_filename);
+    match first_sequence{
+
+        Ok(first_seq) => {
+
+            let second_sequence = read_file_lines(second_filename);
+            match second_sequence{
+                Ok(second_seq) => {
+
+                    let longest_common_sequence = lcs::Lcs::new(&first_seq, &second_seq);
+                    longest_common_sequence.diff();
+
+                },
+                Err(e) => panic!("[ERR] Unable to open file {}. {:?}", second_filename, e.to_string())
+            };
+
+        },
+        Err(e) => panic!("[ERR] Unable to open file {}. {:?}", first_filename, e.to_string())
+    };
 }
